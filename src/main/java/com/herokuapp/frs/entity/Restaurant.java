b00130b239fe5c35c755795f9ab999e134b600c3
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-13 12:35:10 -0700

    add restaurant entity

---
+++ src/main/java/com/herokuapp/frs/entity/Restaurant.java
@@ -0,0 +1,73 @@
+package com.herokuapp.frs.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="restaurant")
+public class Restaurant{
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @Column(name="id")
+  private int id;
+
+  @Column(name="name")
+  private String name;
+
+  @Column(name="logo_path")
+  private String logoPath;
+
+  @Column(name="primary_color")
+  private String primaryColor;
+
+  @Column(name="secondary_color")
+  private String secondaryColor;
+
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLogoPath() {
+    return logoPath;
+  }
+
+  public void setLogoPath(String logoPath) {
+    this.logoPath = logoPath;
+  }
+
+  public String getPrimaryColor() {
+    return primaryColor;
+  }
+
+  public void setPrimaryColor(String primaryColor) {
+    this.primaryColor = primaryColor;
+  }
+
+  public String getSecondaryColor() {
+    return secondaryColor;
+  }
+
+  public void setSecondaryColor(String secondaryColor) {
+    this.secondaryColor = secondaryColor;
+  }
+
+}
