ebc5f1e310340bc3eb36f92f06e183272c0f9c1c
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-13 20:01:09 -0700

    add user dao

---
+++ src/main/java/com/herokuapp/frs/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package com.herokuapp.frs.dao;
+
+import java.util.List;
+
+import com.herokuapp.frs.entity.User;
+
+public interface UserDAO {
+
+  public List<User> getUsers();
+
+  public User getUser(int id);
+
+  public void saveUser(User user);
+}
---
+++ src/main/java/com/herokuapp/frs/dao/UserDAOImpl.java
@@ -0,0 +1,38 @@
+package com.herokuapp.frs.dao;
+
+import java.util.List;
+
+import com.herokuapp.frs.entity.User;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+  @Autowired
+  private SessionFactory sessionFactory;
+
+  @Override
+  public List<User> getUsers() {
+    Session session = sessionFactory.getCurrentSession();
+    Query<User> query = session.createQuery("from User", User.class);
+    return query.getResultList();
+  }
+
+  @Override
+  public User getUser(int id) {
+    Session session = sessionFactory.getCurrentSession();
+     return session.get(User.class, id);
+  }
+
+	@Override
+	public void saveUser(User user) {
+    Session session = sessionFactory.getCurrentSession();
+    session.saveOrUpdate(user);
+	}
+
+}
