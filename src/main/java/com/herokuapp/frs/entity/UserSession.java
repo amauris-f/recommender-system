46bf591b3701d275291db6cf2c4a270b0de56cc4
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-13 20:06:03 -0700

    add schema to Entity

--- src/main/java/com/herokuapp/frs/entity/UserSession.java
+++ src/main/java/com/herokuapp/frs/entity/UserSession.java
@@ -9,7 +9,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="user_session" schema="recommender_system")
+@Table(name="user_session", schema="recommender_system")
 public class UserSession{
 
   @Id
