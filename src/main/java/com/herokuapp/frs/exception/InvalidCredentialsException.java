43df6c896fc8d971887b323de81394e86da0c168
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-14 23:23:03 -0700

    add unauthorized exception handling

---
+++ src/main/java/com/herokuapp/frs/exception/InvalidCredentialsException.java
@@ -0,0 +1,18 @@
+package com.herokuapp.frs.exception;
+
+public class InvalidCredentialsException extends RuntimeException{
+
+  private static final long serialVersionUID = 1L;
+
+  public InvalidCredentialsException(String message, Throwable cause){
+    super(message, cause);
+  }
+
+  public InvalidCredentialsException(String message){
+    super(message);
+  }
+
+  public InvalidCredentialsException(Throwable cause){
+    super(cause);
+  }
+}
---
+++ src/main/java/com/herokuapp/frs/rest/UserExceptionHandling.java
@@ -0,0 +1,26 @@
+package com.herokuapp.frs.rest;
+
+import javax.activity.InvalidActivityException;
+
+import com.herokuapp.frs.exception.InvalidCredentialsException;
+import com.herokuapp.frs.exception.InvalidCredentialsResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class UserExceptionHandling{
+
+  @ExceptionHandler
+  public ResponseEntity<InvalidCredentialsResponse> invalidException(InvalidCredentialsException exc){
+
+    InvalidCredentialsResponse response = new InvalidCredentialsResponse();
+    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+    response.setMessage(exc.getMessage());
+    response.setTimeStamp(System.currentTimeMillis());
+
+    return new ResponseEntity<InvalidCredentialsResponse>(response, HttpStatus.UNAUTHORIZED);
+  }
+}
