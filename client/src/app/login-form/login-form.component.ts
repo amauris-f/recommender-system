18c40d025384d07529ad4c3f53a7dd13a3fd9f16
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-16 01:34:30 -0700

    show message if credentials are incorrect

--- client/src/app/login-form/login-form.component.ts
+++ client/src/app/login-form/login-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 import { User } from '../user';
 import { NgForm } from '@angular/forms';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { ErrorResponseBody } from '../error-response-body';
 
 @Component({
   selector: 'app-login-form',
@@ -12,6 +14,7 @@ export class LoginFormComponent implements OnInit {
 
   constructor(private authenticationService: AuthenticationService) { }
   private user : User;
+  public message : String;
 
   ngOnInit() {
   }
@@ -19,6 +22,12 @@ export class LoginFormComponent implements OnInit {
   onSubmit(ngform : NgForm){
     this.authenticationService.login(ngform.value).subscribe(user =>{
       localStorage.setItem("user", JSON.stringify(user));
+    },
+    (err: HttpErrorResponse) => {
+      if (err.status == 401){
+        var body = err.error as ErrorResponseBody;
+        this.message=body.message;
+      }
     });
   }
 
