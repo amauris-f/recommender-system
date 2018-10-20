049f3ff5b200d1f9ff334129d7ee4712f6d83e1b
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-20 14:59:45 -0700

    angular build changes

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -30,7 +30,7 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ".navbar {\r\n  background-color: #CF1E00;\r\n}"
+module.exports = ""
 
 /***/ }),
 
@@ -41,7 +41,7 @@ module.exports = ".navbar {\r\n  background-color: #CF1E00;\r\n}"
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<nav class=\"navbar sticky-top navbar-dark\">\n  <a class=\"navbar-brand\" href=\"#\">Food Recommender System</a>\n</nav>\n\n<app-login-form *ngIf=\"!isAuthenticated(); else helloTemplate \"></app-login-form>\n<ng-template #helloTemplate>\n  <div>\n    <h1>Hello User!</h1>\n  </div>\n</ng-template>"
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<app-navbar></app-navbar>\n\n<app-login-form *ngIf=\"!isAuthenticated(); else helloTemplate \"></app-login-form>\n<ng-template #helloTemplate>\n  <div>\n    <h1>Hello User!</h1>\n  </div>\n</ng-template>"
 
 /***/ }),
 
@@ -104,12 +104,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 /* harmony import */ var _login_form_login_form_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./login-form/login-form.component */ "./src/app/login-form/login-form.component.ts");
+/* harmony import */ var _navbar_navbar_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./navbar/navbar.component */ "./src/app/navbar/navbar.component.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
 
 
 
@@ -123,7 +125,8 @@ var AppModule = /** @class */ (function () {
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
             declarations: [
                 _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"],
-                _login_form_login_form_component__WEBPACK_IMPORTED_MODULE_5__["LoginFormComponent"]
+                _login_form_login_form_component__WEBPACK_IMPORTED_MODULE_5__["LoginFormComponent"],
+                _navbar_navbar_component__WEBPACK_IMPORTED_MODULE_6__["NavbarComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
@@ -176,6 +179,17 @@ var AuthenticationService = /** @class */ (function () {
     }
     AuthenticationService.prototype.login = function (login) {
         return this.httpClient.post(this.loginUrl, login, this.httpOptions);
+    };
+    AuthenticationService.prototype.isAuthenticated = function () {
+        if (document.cookie.indexOf("JSESSION") >= 0) {
+            this.user = JSON.parse(localStorage.getItem("user"));
+            return true;
+        }
+        localStorage.removeItem("user");
+        return false;
+    };
+    AuthenticationService.prototype.getUser = function () {
+        return this.user;
     };
     AuthenticationService = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Injectable"])({
@@ -261,6 +275,72 @@ var LoginFormComponent = /** @class */ (function () {
         __metadata("design:paramtypes", [_authentication_service__WEBPACK_IMPORTED_MODULE_1__["AuthenticationService"]])
     ], LoginFormComponent);
     return LoginFormComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/navbar/navbar.component.css":
+/*!*********************************************!*\
+  !*** ./src/app/navbar/navbar.component.css ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".navbar {\r\n  background-color: #CF1E00;\r\n}\r\n\r\ndiv {\r\n  color: white;\r\n}"
+
+/***/ }),
+
+/***/ "./src/app/navbar/navbar.component.html":
+/*!**********************************************!*\
+  !*** ./src/app/navbar/navbar.component.html ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<nav class=\"navbar sticky-top navbar-dark\">\n  <a class=\"navbar-brand\" href=\"#\">Food Recommender System</a>\n  <div *ngIf=\"authService.isAuthenticated()\">\n    <p>Hello, {{authService.getUser().username}}</p>\n  </div>\n</nav>\n"
+
+/***/ }),
+
+/***/ "./src/app/navbar/navbar.component.ts":
+/*!********************************************!*\
+  !*** ./src/app/navbar/navbar.component.ts ***!
+  \********************************************/
+/*! exports provided: NavbarComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "NavbarComponent", function() { return NavbarComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _authentication_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../authentication.service */ "./src/app/authentication.service.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var NavbarComponent = /** @class */ (function () {
+    function NavbarComponent(authService) {
+        this.authService = authService;
+    }
+    NavbarComponent.prototype.ngOnInit = function () {
+    };
+    NavbarComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-navbar',
+            template: __webpack_require__(/*! ./navbar.component.html */ "./src/app/navbar/navbar.component.html"),
+            styles: [__webpack_require__(/*! ./navbar.component.css */ "./src/app/navbar/navbar.component.css")]
+        }),
+        __metadata("design:paramtypes", [_authentication_service__WEBPACK_IMPORTED_MODULE_1__["AuthenticationService"]])
+    ], NavbarComponent);
+    return NavbarComponent;
 }());
 
 
