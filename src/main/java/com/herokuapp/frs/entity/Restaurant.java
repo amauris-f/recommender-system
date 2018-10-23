1e78a0c90e349d218bac9713d3f89d607584ef2a
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-23 14:44:25 -0700

    remove unnecessary imports

--- src/main/java/com/herokuapp/frs/entity/Restaurant.java
+++ src/main/java/com/herokuapp/frs/entity/Restaurant.java
@@ -12,9 +12,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 
 @Entity
