e15aa382d2fdfc0c756d9ffcd9fe5759ab79c3f6
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-23 14:37:49 -0700

    add autogeneration id

--- src/main/java/com/herokuapp/frs/entity/UserSession.java
+++ src/main/java/com/herokuapp/frs/entity/UserSession.java
@@ -3,6 +3,8 @@ package com.herokuapp.frs.entity;
 import java.time.ZonedDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -13,6 +15,7 @@ import javax.persistence.Table;
 public class UserSession{
 
   @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
   @Column(name="id")
   private int id;
 
