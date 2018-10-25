5ebeb889ffe84e76819070ad08c759462a216b6d
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-24 19:48:30 -0700

    add repository annotation to restaruantDAO

--- src/main/java/com/herokuapp/frs/dao/RestaurantDAOImpl.java
+++ src/main/java/com/herokuapp/frs/dao/RestaurantDAOImpl.java
@@ -5,7 +5,9 @@ import com.herokuapp.frs.entity.Restaurant;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RestaurantDAOImpl implements RestaurantDAO {
   @Autowired
   SessionFactory sessionFactory;
