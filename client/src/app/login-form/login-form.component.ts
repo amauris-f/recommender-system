e26a1d0d0647d5958d53ca0460efcff59dc0a207
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-27 22:13:05 -0700

    redirect after login

--- client/src/app/login-form/login-form.component.ts
+++ client/src/app/login-form/login-form.component.ts
@@ -4,6 +4,7 @@ import { User } from '../user';
 import { NgForm } from '@angular/forms';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { ErrorResponseBody } from '../error-response-body';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -12,7 +13,7 @@ import { ErrorResponseBody } from '../error-response-body';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService, private router : Router) { }
   private user : User;
   public message : String;
 
@@ -22,6 +23,7 @@ export class LoginFormComponent implements OnInit {
   onSubmit(ngform : NgForm){
     this.authenticationService.login(ngform.value).subscribe(user =>{
       localStorage.setItem("user", JSON.stringify(user));
+      this.router.navigate(['']);
     },
     (err: HttpErrorResponse) => {
       if (err.status == 401){
