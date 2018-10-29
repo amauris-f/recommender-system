8c56ff934523c97ad832ad8533876a2b5219cfed
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-10-29 15:44:42 -0700

    get user reviews in dashboard component

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { User } from '../user';
+import { Review } from '../review';
+import { ReviewService } from '../api-services/review.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  reviews: Review[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private reviewService: ReviewService) {
   }
 
+  ngOnInit() {
+    this.getRecentReviews();
+  }
+
+  getRecentReviews() {
+    this.reviewService.getRecentReviews().subscribe(reviews =>
+      this.reviews = reviews
+    );
+  }
 }
