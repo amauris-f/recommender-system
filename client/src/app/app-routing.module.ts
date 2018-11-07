0bb2957e02833b51175984cae3db274eedbeb58f
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-11-06 21:17:24 -0800

    add new routes to handle restaurant components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuardService } from './auth-guard.service';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 
 const routes:  Routes = [
   { path: 'login', component: LoginFormComponent },
-  { path: '', component: DashboardComponent,  canActivate: [AuthGuardService] }
-] 
+  { path: '', component: DashboardComponent,  canActivate: [AuthGuardService] },
+  { path: 'restaurant', component: RestaurantListComponent,  canActivate: [AuthGuardService] },
+  { path: 'restaurant/:id', component: RestaurantDetailComponent, canActivate: [AuthGuardService]}
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
