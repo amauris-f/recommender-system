3bf28619441a85e8b8baad66f340cc7493b4f96e
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-11-06 21:28:21 -0800

    add constructor with optional parameters

--- client/src/app/review.ts
+++ client/src/app/review.ts
@@ -2,6 +2,12 @@ import { User } from "./user";
 import { Item } from "./item";
 
 export class Review {
+
+  public constructor(user?: User , item?: Item, rating?: Number){
+    this.user=user;
+    this.item=item;
+    this.rating=rating;
+  }
   public id: Number;
 
   public user: User;
