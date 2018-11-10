89e90b108a2197bb40f913eb14002d0f65203e1f
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-11-10 10:12:53 -0800

    save review by calling api

--- client/src/app/api-services/review.service.ts
+++ client/src/app/api-services/review.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Review } from '../review';
 import { Observable } from 'rxjs';
+import { ResponseBody } from '../response-body';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class ReviewService {
   
   constructor(private http: HttpClient, private authService: AuthenticationService) { }
   private userUrl = '/api/users';
+  private reviewUrl = '/api/reviews';
 
   getRecentReviews(): Observable<Review[]>{
     var userId = this.authService.getUser().id;
@@ -22,5 +24,16 @@ export class ReviewService {
     var userId = this.authService.getUser().id;
     const url = `${this.userUrl}/${userId}/reviews/restaurants/${restId}`;
     return this.http.get<Review[]>(url);
+  }
+
+  saveReview(review: Review): Observable<ResponseBody>{
+    var authUser= this.authService.getUser();
+    review.user = authUser;
+    if(review.id){
+      var url = `${this.reviewUrl}/${authUser.id}`;
+      return this.http.put<ResponseBody>(url, review);
+    } else {
+      return this.http.post<ResponseBody>(this.reviewUrl, review);
+    }
   }
 }
