404755cc615a7813fafd610fd1c94f8c16dd74a0
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-11-10 10:16:18 -0800

    return message to client in response body when review save is successful

--- src/main/java/com/herokuapp/frs/rest/ReviewController.java
+++ src/main/java/com/herokuapp/frs/rest/ReviewController.java
@@ -4,9 +4,13 @@ package com.herokuapp.frs.rest;
 import javax.servlet.http.HttpServletRequest;
 
 import com.herokuapp.frs.entity.Review;
+import com.herokuapp.frs.exception.OKResponse;
 import com.herokuapp.frs.service.ReviewService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,16 +25,19 @@ public class ReviewController extends com.herokuapp.frs.rest.RestController {
   private ReviewService reviewService;
 
   @PostMapping("/reviews")
-  public void createReview(@RequestBody Review review, HttpServletRequest request){
+  public ResponseEntity<OKResponse> createReview(@RequestBody Review review, HttpServletRequest request){
     authorizeRequest(request, review.getUser());
-    review.setId(0);
     reviewService.saveReview(review);
+    OKResponse response = new OKResponse(HttpStatus.OK.value(),"Response was saved!");
+    return new ResponseEntity<OKResponse>(response, HttpStatus.OK);    
   }
 
-  @PutMapping("/reviews")
-  public void updateReview(@RequestBody Review review, HttpServletRequest request){
-    authorizeRequest(request, review.getUser());
+  @PutMapping("/reviews/{userId}")
+  public ResponseEntity<OKResponse> updateReview(@RequestBody Review review, @PathVariable int userId, HttpServletRequest request){
+    authorizeRequest(request, userId);
     reviewService.saveReview(review);
+    OKResponse response = new OKResponse(HttpStatus.OK.value(),"Response was saved!");
+    return new ResponseEntity<OKResponse>(response, HttpStatus.OK);
   }
 
 }
