b6b6dc34c53e359b9055142725d1f164f25e2f83
Amauris Ferreira <ar.ferreira17@gmail.com>
2018-11-10 18:34:32 -0800

    add signup feature

---
+++ client/src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorResponseBody } from '../error-response-body';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent implements OnInit {
+  constructor(private authenticationService: AuthenticationService, private router : Router) {   }
+  public message : String;
+
+  ngOnInit() {
+  }
+
+  onSubmit(ngform : NgForm){
+    if(ngform.value["password"] !== ngform.value["confirmPassword"]){
+      this.message="Passwords do not match";
+      return;
+    }
+    this.authenticationService.signup(ngform.value).subscribe(user =>{
+      localStorage.setItem("user", JSON.stringify(user));
+      this.router.navigate(['']);
+    },
+    (err: HttpErrorResponse) => {
+      if (err.status == 401){
+        var body = err.error as ErrorResponseBody;
+        this.message=body.message;
+      }
+    });
+  }
+
+}
---
+++ client/src/app/signup-model.ts
@@ -0,0 +1,8 @@
+export class SignupModel {
+  public username: String;
+  public email: String;
+  public password: String;
+  public confirmPassword: String;
+
+  constructor(){};
+}
