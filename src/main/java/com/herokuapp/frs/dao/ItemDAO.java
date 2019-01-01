57af30bfde2d639463b9e8d1cf87b143b3a841cc
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-01-01 14:46:57 -0800

    use native SQL sent from service to implement collaborative filtering

--- src/main/java/com/herokuapp/frs/dao/ItemDAO.java
+++ src/main/java/com/herokuapp/frs/dao/ItemDAO.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import com.herokuapp.frs.entity.Item;
 
+import org.hibernate.query.NativeQuery;
+
 public interface ItemDAO {
   public Item getItem(int itemId);
+  public List<Item> getRecommendedItems(List<Integer> itemIds, int restaurantId);
   public List<Item> getMenu(int restaurantId);
+  public void ddlQuery(String statement);
+  public NativeQuery<?> selectQuery(String statement);
 }
--- src/main/java/com/herokuapp/frs/dao/ItemDAOImpl.java
+++ src/main/java/com/herokuapp/frs/dao/ItemDAOImpl.java
@@ -6,6 +6,7 @@ import com.herokuapp.frs.entity.Item;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -21,11 +22,31 @@ public class ItemDAOImpl implements ItemDAO {
     return currentSession.get(Item.class, itemId);
   }
 
+  @Override
+  public List<Item> getRecommendedItems(List<Integer> itemIds, int restaurant_id) {
+    Session currentSession = sessionFactory.getCurrentSession();
+    String hql = "FROM Item WHERE id IN :itemIds AND restaurant_id=" + restaurant_id;
+    List<Item> items = currentSession.createQuery(hql, Item.class).setParameterList("itemIds", itemIds).getResultList();
+    return items;
+  }
+
 	@Override
 	public List<Item> getMenu(int restaurantId) {
     Session currentSession = sessionFactory.getCurrentSession();
     String hql = "FROM Item WHERE restaurant_id=" + restaurantId;
     return currentSession.createQuery(hql, Item.class).getResultList();
 	}
+
+  @Override
+  public void ddlQuery(String statement) {
+    Session currentSession = sessionFactory.getCurrentSession();
+    currentSession.createNativeQuery(statement).executeUpdate();
+  }
+
+  @Override
+  public NativeQuery<?> selectQuery(String statement) {
+    Session currentSession = sessionFactory.getCurrentSession();
+    return currentSession.createNativeQuery(statement);
+  }
 
 }
