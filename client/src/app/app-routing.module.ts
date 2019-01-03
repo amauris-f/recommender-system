360da4d09ce183cec6cca57e03013a1a433bc9df
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-01-02 18:33:33 -0800

    add 404 route and forward any unknown routes to 404

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,14 +7,17 @@ import { RestaurantListComponent } from './restaurant-list/restaurant-list.compo
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { RecommendationListComponent } from './recommendation-list/recommendation-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes:  Routes = [
-  { path: 'login', component: LoginFormComponent },
-  { path: 'signup', component: SignupFormComponent },
+  { path: 'login', component: LoginFormComponent},
+  { path: 'signup', component: SignupFormComponent},
   { path: '', component: DashboardComponent,  canActivate: [AuthGuardService] },
   { path: 'restaurant', component: RestaurantListComponent,  canActivate: [AuthGuardService] },
   { path: 'restaurant/:id', component: RestaurantDetailComponent, canActivate: [AuthGuardService]},
-  { path: 'recommend', component: RecommendationListComponent, canActivate: [AuthGuardService]}
+  { path: 'recommend', component: RecommendationListComponent, canActivate: [AuthGuardService]},
+  { path: '404', component: NotFoundComponent},
+  { path: '**', redirectTo: '/404'}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
